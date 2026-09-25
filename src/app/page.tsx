8ab9f91c509f03b40fce455d16e2521fc10b4df4
afiaafia <afiaanna68@gmail.com>
2026-09-25 18:21:45 +0600

    chore: use canonical tailwind width utility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function HomePage() {
 
       <section
         id="library"
-        className="mx-auto w-full max-w-6xl px-4 pb-16 pt-10 sm:px-6 lg:px-8"
+        className="mx-auto w-full max-w-300 px-4 pb-16 pt-10 sm:px-6 lg:px-8"
       >
         {/* Library Header */}
         <div className="mb-6">
